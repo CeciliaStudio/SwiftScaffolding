cb7b109457ca04c62c64ffa03a1e9ed37904fb63
YiZhiMCQiu <yizhimcqiu@icloud.com>
2025-10-27 12:42:05 +0800

    feat: 房间码生成

--- Sources/SwiftScaffolding/Scaffolding.swift
+++ Sources/SwiftScaffolding/Scaffolding.swift
@@ -11,16 +11,57 @@ import CryptoKit
 import Network
 
 public final class Scaffolding {
-    /// 根据设备的主板唯一标识符生成设备码。
-    /// - Returns: 设备码。
+    /// 根据设备的主板唯一标识符生成设备标识符。
+    /// https://github.com/Scaffolding-MC/Scaffolding-MC/blob/main/README.md#machine_id
+    /// - Returns: 设备标识符。
     public static func getMachineID() -> String {
         let service: io_service_t = IOServiceGetMatchingService(kIOMainPortDefault, IOServiceMatching("IOPlatformExpertDevice"))
         let uuid: String = IORegistryEntryCreateCFProperty(service, "IOPlatformUUID" as CFString, kCFAllocatorDefault, 0).takeUnretainedValue() as? String ?? UUID().uuidString
         return Insecure.SHA1.hash(data: Data(uuid.utf8)).map { String(format: "%02hhx", $0) }.joined()
     }
     
+    /// 生成符合 Scaffolding 标准的房间码。
+    /// https://github.com/Scaffolding-MC/Scaffolding-MC/blob/main/README.md#联机房间码
+    /// - Returns: 生成的房间码。
+    public static func generateRoomCode() -> String {
+        let charset: [Character] = Array("0123456789ABCDEFGHJKLMNPQRSTUVWXYZ")
+        let mapping: [Character: Int] = Dictionary(uniqueKeysWithValues: charset.enumerated().map { ($1, $0) })
+        
+        func randomChar() -> Character {
+            var byte: UInt8 = 0
+            repeat {
+                byte = UInt8.random(in: 0..<34)
+            } while byte >= charset.count
+            return charset[Int(byte)]
+        }
+        
+        while true {
+            var code: [Character] = []
+            code.append("U")
+            code.append("/")
+            for _ in 0..<4 { code.append(randomChar()) }
+            code.append("-")
+            for _ in 0..<4 { code.append(randomChar()) }
+            code.append("-")
+            for _ in 0..<4 { code.append(randomChar()) }
+            code.append("-")
+            for _ in 0..<4 { code.append(randomChar()) }
+            
+            let codeChars: [Character] = Array(code[2...5] + code[7...10] + code[12...15] + code[17...20])
+            let nums: [Int] = codeChars.map { mapping[$0] ?? 0 }
+            
+            var remainder: Int = 0
+            for n in nums {
+                remainder = (remainder * 34 + n) % 7
+            }
+            if remainder == 0 {
+                return String(code)
+            }
+        }
+    }
+    
     /// 发送 Scaffolding 请求。
-    /// https://github.com/Scaffolding-MC/Scaffolding-MC?tab=readme-ov-file#联机信息获取协议
+    /// https://github.com/Scaffolding-MC/Scaffolding-MC/blob/main/README.md#联机信息获取协议
     /// - Parameters:
     ///   - type: 请求类型。
     ///   - connection: 到联机中心的连接。
