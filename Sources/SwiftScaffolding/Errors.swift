5eb7ed8272ad034de6c3d7ad87f6fdc49d4b6d8d
AnemoFlower <yizhimcqiu@icloud.com>
2026-01-27 11:51:32 +0800

    feat: 为 ConnectionError 添加本地化

--- Sources/SwiftScaffolding/Errors.swift
+++ Sources/SwiftScaffolding/Errors.swift
@@ -7,12 +7,41 @@
 
 import Foundation
 
-public enum ConnectionError: Error {
+public enum ConnectionError: LocalizedError {
     case invalidPort
     case timeout
     case cancelled
     case missingConnection
     case invalidConnectionState
+    
+    public var errorDescription: String? {
+        switch self {
+        case .invalidPort:
+            return NSLocalizedString(
+                "ConnectionError.invalidPort",
+                bundle: Bundle.module,
+                comment: "联机中心返回了一个非法端口号"
+            )
+        case .timeout:
+            return NSLocalizedString(
+                "ConnectionError.timeout",
+                bundle: Bundle.module,
+                comment: "接收数据或建立连接超时"
+            )
+        case .cancelled:
+            return NSLocalizedString(
+                "ConnectionError.connectionClosed",
+                bundle: Bundle.module,
+                comment: "连接被关闭"
+            )
+        case .missingConnection, .invalidConnectionState:
+            return NSLocalizedString(
+                "ConnectionError.connectionIsNotReady",
+                bundle: Bundle.module,
+                comment: "未建立连接，或者连接状态异常"
+            )
+        }
+    }
 }
 
 public enum RoomCodeError: Error {
